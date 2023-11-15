a87729370117b52e378331f8ce1fe74671c595ae
Florent Biville <florent.biville@neo4j.com>
2023-11-15 16:49:21 +0100

    Push initial version of local googlecloud-to-neo4j runner

---
+++ README.md
@@ -0,0 +1,107 @@
+# Run your `googlecloud-to-neo4j` pipeline locally
+
+## Prerequisites
+
+ - Java 21
+ - Apache Maven
+ - Docker
+ - GCP account with Dataflow enabled
+ - GCS bucket accessible for writes
+
+## Building the CLI
+
+First, build the `googlecloud-to-neo4j` template locally:
+
+```shell
+git clone git@github.com:GoogleCloudPlatform/DataflowTemplates.git
+mvn --file DataflowTemplates/pom.xml --also-make --projects v2/googlecloud-to-neo4j install -DskipTests -Djib.skip
+```
+
+Then, go back to this project and run:
+
+```shell
+mvn package
+```
+
+You should then be able to run:
+
+```shell
+java -jar target/local-runner-1.0-SNAPSHOT-shaded.jar --help
+```
+
+And see some output similar to:
+```shell
+Usage: local-dataflow [-hV] -b=<bucket> [-i=<checkInterval>] -p=<project>
+                      -r=<region> -s=<spec> [-t=<maxTimeout>]
+  -b, --bucket=<bucket>     GCS bucket
+  -h, --help                Show this help message and exit.
+  -i, --interval-check-duration=<checkInterval>
+                            Execution completion check interval
+  -p, --project=<project>   GCP project
+  -r, --region=<region>     GCP region
+  -s, --spec=<spec>         Path to local googlecloud-to-neo4j spec file
+  -t, --max-timeout=<maxTimeout>
+                            Execution timeout
+  -V, --version             Print version information and exit.
+```
+
+## Quick start
+
+For the guide, you will need:
+
+ - to have built the CLI locally (see previous section)
+ - to know your GCP project name
+ - to pick a GCS bucket name accessible for writes
+ - a running Docker Daemon
+
+Create a local spec file, let's save it somewhere (the rest of the guide assumes `/path/to/spec.json`):
+
+```json
+{
+  "sources": [
+    {
+      "type": "text",
+      "format": "EXCEL",
+      "name": "persons",
+      "ordered_field_names": "id",
+      "data": [
+        ["person0"],
+        ["person1"],
+        ["person2"],
+        ["person3"],
+        ["person4"]
+      ]
+    }
+  ],
+  "targets": [
+    {
+      "node": {
+        "source": "persons",
+        "name": "person import",
+        "mode": "merge",
+        "mappings": {
+          "labels": [
+            "\"Person\""
+          ],
+          "properties": {
+            "keys": [
+              {"id": "id"}
+            ]
+          }
+        }
+      }
+    }
+  ]
+}
+```
+
+Assuming the current location is the root of this project, now run:
+```shell
+java -jar ./target/local-runner-1.0-SNAPSHOT-shaded.jar \
+  --project=<YOUR GCP PROJECT> \
+  --region=<YOUR GCP REGION> \
+  --bucket=<YOUR GCS BUCKET> \
+  --spec=/path/to/spec.json
+```
+And that's it!
+A local Neo4j instance is going to be started via Docker and the pipeline will run directly on your machine. 
---
+++ src/main/java/org/neo4j/dataflow/LocalRunner.java
@@ -0,0 +1,150 @@
+package org.neo4j.dataflow;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.auth.oauth2.ServiceAccountCredentials;
+import com.google.cloud.teleport.v2.neo4j.templates.GoogleCloudToNeo4j;
+import org.apache.beam.it.common.PipelineLauncher;
+import org.apache.beam.it.common.PipelineLauncher.JobState;
+import org.apache.beam.it.common.PipelineLauncher.LaunchConfig;
+import org.apache.beam.it.common.PipelineLauncher.LaunchInfo;
+import org.apache.beam.it.common.PipelineOperator;
+import org.apache.beam.it.common.PipelineOperator.Config;
+import org.apache.beam.it.common.utils.ResourceManagerUtils;
+import org.apache.beam.it.gcp.artifacts.utils.ArtifactUtils;
+import org.apache.beam.it.gcp.dataflow.DirectRunnerClient;
+import org.apache.beam.it.gcp.storage.GcsResourceManager;
+import org.apache.beam.it.neo4j.Neo4jResourceManager;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.time.Duration;
+import java.util.Random;
+
+@Command(name = "local-dataflow", version = "0.0.1", mixinStandardHelpOptions = true)
+public class LocalRunner implements Runnable, AutoCloseable {
+
+    private GcsResourceManager gcs;
+    private Neo4jResourceManager neo4j;
+    private PipelineLauncher launcher;
+    private LaunchInfo execution;
+
+    @Option(names = {"--region", "-r"}, description = "GCP region", required = true)
+    String region;
+    @Option(names = {"--project", "-p"}, description = "GCP project", required = true)
+    String project;
+    @Option(names = {"--bucket", "-b"}, description = "GCS bucket", required = true)
+    String bucket;
+    @Option(names = {"--spec", "-s"}, description = "Path to local googlecloud-to-neo4j spec file", required = true)
+    File spec;
+    @Option(names = {"--max-timeout", "-t"}, description = "Execution timeout")
+    Duration maxTimeout = Duration.ofMinutes(5);
+    @Option(names = {"--interval-check-duration", "-i"}, description = "Execution completion check interval")
+    Duration checkInterval = Duration.ofSeconds(5);
+
+    public static void main(String[] args) {
+        var localRunner = new LocalRunner();
+        Runtime.getRuntime().addShutdownHook(new Thread(localRunner::close));
+        System.exit(new CommandLine(localRunner).execute(args));
+    }
+
+    @Override
+    public void run() {
+        GoogleCredentials credentials = credentials();
+        gcs = GcsResourceManager.builder(bucket, getClass().getSimpleName(), credentials).build();
+        neo4j = Neo4jResourceManager.builder(executionId())
+                .setAdminPassword("letmein!")
+                .build();
+
+        uploadTemplateInputs();
+        execution = startPipeline(credentials);
+        waitUntilDone();
+    }
+
+    @Override
+    public void close() {
+        stopPipeline();
+        ResourceManagerUtils.cleanResources(gcs, neo4j);
+    }
+
+    private void uploadTemplateInputs() {
+        try {
+            gcs.createArtifact("spec.json", Files.readString(spec.toPath()));
+            gcs.createArtifact("neo4j.json", localConnectionMetadata());
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to upload template inputs", e);
+        }
+    }
+
+    private LaunchInfo startPipeline(GoogleCredentials credentials) {
+        launcher = DirectRunnerClient.builder(GoogleCloudToNeo4j.class).setCredentials(credentials).build();
+        try {
+            return launcher.launch(project, region, LaunchConfig.builder(getClass().getSimpleName(), null)
+                    .addParameter("jobSpecUri", gcsPath("spec.json"))
+                    .addParameter("neo4jConnectionUri", gcsPath("neo4j.json"))
+                    .addParameter("tempLocation", "gs://" + bucket + "/temp/")
+                    .build());
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to start job", e);
+        }
+    }
+
+    private void stopPipeline() {
+        if (launcher == null || execution == null) {
+            return;
+        }
+        if (JobState.FINISHING_STATES.contains(execution.state())) {
+            return;
+        }
+        try {
+            launcher.cancelJob(execution.projectId(), execution.region(), execution.jobId());
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to stop job " + execution.jobId(), e);
+        }
+    }
+
+    private static GoogleCredentials credentials() {
+        try {
+            return ServiceAccountCredentials.getApplicationDefault();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static String executionId() {
+        var suffix = new Random().ints('a', 'z' + 1)
+                .limit(8)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+        return String.format("local-runner-%d-%s", System.currentTimeMillis(), suffix);
+    }
+
+    private void waitUntilDone() {
+        new PipelineOperator(launcher)
+                .waitUntilDone(Config.builder()
+                        .setJobId(execution.jobId())
+                        .setProject(project)
+                        .setRegion(region)
+                        .setTimeoutAfter(maxTimeout).setCheckAfter(checkInterval)
+                        .build());
+    }
+
+    private String gcsPath(String artifactId) {
+        return ArtifactUtils.getFullGcsPath(bucket, getClass().getSimpleName(), gcs.runId(), artifactId);
+    }
+
+    private String localConnectionMetadata() {
+        return """
+                {
+                  "server_url": "%s",
+                  "database": "%s",
+                  "auth_type": "basic",
+                  "username": "neo4j",
+                  "pwd": "%s"
+                }
+                """.formatted(neo4j.getUri(), neo4j.getDatabaseName(), neo4j.getAdminPassword());
+    }
+}
